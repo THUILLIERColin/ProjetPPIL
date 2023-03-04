0856419412ff81a454860c72852b72d7e552ce6b
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2023-03-04 12:40:58 +0100

    Ajout de Couleur
    À voir pour peut-être le supprimer, voir comment ça peut marcher avec un int

---
+++ ClienCpp/Formes/Couleur.h
@@ -0,0 +1,21 @@
+/**
+ * @author THUILLIER Colin
+ */
+
+#pragma once
+
+/**
+ * Cette classe enum permet de définir une couleur a partir de valeurs hexadécimales
+ * Elle est faite pour simplifier l'utilisation des couleurs dans le programme
+ */
+enum class Couleur {
+    ROUGE = 0xFF0000,
+    VERT = 0x00FF00,
+    BLEU = 0x0000FF,
+    BLANC = 0xFFFFFF,
+    NOIR = 0x000000,
+    JAUNE = 0xFFFF00,
+    CYAN = 0x00FFFF,
+    MAGENTA = 0xFF00FF,
+    GRIS = 0x808080
+};
