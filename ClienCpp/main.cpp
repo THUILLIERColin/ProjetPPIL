86a9aeae3fdf6fe0f0d942c0ea5b001e60656c28
Maelan LEOST <maelan.leost@gmail.com>
2023-03-05 14:12:14 +0100

    changement dans le main
    ajout de tests

--- ClienCpp/main.cpp
+++ ClienCpp/main.cpp
@@ -15,10 +15,67 @@
 int main(){
 
     /**************************************************************************
-     *  Partie 1 : Les tests
+     *  Partie 1 :                   Les tests
      *************************************************************************/
+    printf("****************************************************\n                   TESTS                   \n****************************************************\n\n\n");
 
-    printf("Test sur le polygone\n");
+
+    printf("********************test sur le segment********************\n");
+    Segment segment(1, 1, 2,2, 0xFF0000);
+    cout << segment << endl;
+
+    cout << segment.calculerAire() << endl;
+    cout << segment.getCentreDeSymetrie() << endl;
+    cout << segment.getMinXMinY() << endl;
+    cout << segment.getMaxXMaxY() << endl;
+    segment.translation(Vecteur2D(2,1));
+    printf("\ntest sur le Segment apres translation\n");
+    cout << segment.getCentreDeSymetrie() << endl;
+    cout << segment.getMinXMinY() << endl;
+    cout << segment.getMaxXMaxY() << endl;
+    printf("\ntest sur le Segment apres homothetie\n");
+    segment.homothetie(Vecteur2D(2,1),5);
+    cout << segment.getCentreDeSymetrie() << endl;
+    cout << segment.getMinXMinY() << endl;
+    cout << segment.getMaxXMaxY() << endl;
+    printf("\ntest sur le Segment apres rotation\n");
+    cout << segment.getCentreDeSymetrie() << endl;
+    cout << segment.getMinXMinY() << endl;
+    cout << segment.getMaxXMaxY() << endl;
+
+    printf("\n");
+
+    //test cercle
+    printf("\n");
+    printf("********************test sur le Cercle********************\n");
+    Cercle cercle(1, 2, 1, 0x000000);
+    cout << cercle.calculerAire() << endl;
+    cout << cercle.getCentreDeSymetrie() << endl;
+    cout << cercle.getMinXMinY() << endl;
+    cout << cercle.getMaxXMaxY() << endl;
+    cout << cercle << endl ;
+    //test transaltion
+    cercle.translation(Vecteur2D(2,1));
+    printf("\ntest sur le Cercle apres translation\n");
+    cout << cercle.getCentreDeSymetrie() << endl;
+    cout << cercle.getMinXMinY() << endl;
+    cout << cercle.getMaxXMaxY() << endl;
+    //test homothetie
+    printf("\ntest sur le Cercle apres homothetie\n");
+    cercle.homothetie(Vecteur2D(2,1),5);
+    cout << cercle.getCentreDeSymetrie() << endl;
+    cout << cercle.getMinXMinY() << endl;
+    cout << cercle.getMaxXMaxY() << endl;
+    //test rotation
+    printf("\ntest sur le Cercle apres rotation\n");
+    cercle.rotation(Vecteur2D(2,1),90);
+    cout << cercle.getCentreDeSymetrie() << endl;
+    cout << cercle.getMinXMinY() << endl;
+    cout << cercle.getMaxXMaxY() << endl;
+
+    //on fait la meme pour un polygone
+    printf("\n");
+    printf("********************test sur le polygone********************\n");
     Polygone p((int) Couleur::ROUGE); // il faudra la retransformer en hexa
     cout << "Couleur du polygone (int) : " << p.getCouleur() << endl;
     cout << "Couleur du polygone (hexa) : " << std::hex << p.getCouleur() << endl;
@@ -29,62 +86,35 @@ int main(){
     cout << p.calculerAire() << endl;
     cout << p.getCentreDeSymetrie() << endl;
     cout << p.getNbPoints() << endl;
-
-
-    printf("\n");
-    printf("test sur le segment\n");
-    Segment segment(1, 1, 2,2, 0xFF0000);
-    cout << segment << endl;
-
-    cout << segment.calculerAire() << endl;
-    cout << segment.getCentreDeSymetrie() << endl;
-    cout << segment.getMinXMinY() << endl;
-    cout << segment.getMaxXMaxY() << endl;
-    segment.translation(Vecteur2D(2,1));
-    printf("test sur le Segment apres translation\n");
-    cout << segment.getCentreDeSymetrie() << endl;
-    cout << segment.getMinXMinY() << endl;
-    cout << segment.getMaxXMaxY() << endl;
-    printf("test sur le Segment apres homothetie\n");
-    segment.homothetie(Vecteur2D(2,1),5);
-    cout << segment.getCentreDeSymetrie() << endl;
-    cout << segment.getMinXMinY() << endl;
-    cout << segment.getMaxXMaxY() << endl;
-    printf("test sur le Segment apres rotation\n");
-    cout << segment.getCentreDeSymetrie() << endl;
-    cout << segment.getMinXMinY() << endl;
-    cout << segment.getMaxXMaxY() << endl;
-
-    printf("\n");
-
-    //test cercle
-    printf("\n");
-    printf("test sur le Cercle\n");
-    Cercle cercle(1, 2, 1, 0x000000);
-    cout << cercle.calculerAire() << endl;
-    cout << cercle.getCentreDeSymetrie() << endl;
-    cout << cercle.getMinXMinY() << endl;
-    cout << cercle.getMaxXMaxY() << endl;
-    cout << cercle << endl ;
-
-    //on fait la meme pour un polygone
-    printf("\n");
-    printf("test sur le polygone\n");
-    Polygone p2(0xFF0000);
-    p2.addPoint(Vecteur2D(1,1));
-    p2.addPoint(Vecteur2D(1,2));
-    p2.addPoint(Vecteur2D(2,2));
-    cout << p2 << endl;
+    cout << p << endl;
+    //test transaltion
+    p.translation(Vecteur2D(2,1));
+    printf("\ntest sur le polygone apres translation\n");
+    cout << p.getCentreDeSymetrie() << endl;
+    cout << p.getMinXMinY() << endl;
+    cout << p.getMaxXMaxY() << endl;
+    //test homothetie
+    printf("\ntest sur le polygone apres homothetie\n");
+    p.homothetie(Vecteur2D(2,1),5);
+    cout << p.getCentreDeSymetrie() << endl;
+    cout << p.getMinXMinY() << endl;
+    cout << p.getMaxXMaxY() << endl;
+    //test rotation
+    printf("\ntest sur le polygone apres rotation\n");
+    p.rotation(Vecteur2D(2,1),90);
+    cout << p.getCentreDeSymetrie() << endl;
+    cout << p.getMinXMinY() << endl;
+    cout << p.getMaxXMaxY() << endl;
 
     //on fait un essai de forme complexe
     printf("\n");
-    printf("test sur la forme complexe\n");
+    printf("********************test sur la forme complexe********************\n");
     FormeComplexe formeComplexe(0x000000);
     formeComplexe.ajouterForme(&cercle);
     formeComplexe.ajouterForme(&segment);
     formeComplexe.ajouterForme(&p);
     cout << formeComplexe << endl;
-
+    printf("\n");
 
     /**************************************************************************
      *  Partie 2 : Le réseau
@@ -114,11 +144,11 @@ int main(){
     /**************************************************************************
      *  Partie 4: Les test sur le COR
      *************************************************************************/
-    printf("*************test sur le COR***********");
-    //test segment
+    printf("****************************************************\n                   TESTS COR                   \n****************************************************\n\n\n");
     ExpertChargementCOR* expertSuivant ;
-    printf("\n");
-    printf("test sur le COR  segment\n");
+
+    //test segment
+    printf("********************test sur le COR  segment********************\n");
     Segment segmentCOR(1, 1, 2,2, 0xFF0000);
     cout << segmentCOR << endl;
     ifstream fichier("COR/FichiersTest/TestCORSegment.txt");
@@ -129,7 +159,7 @@ int main(){
 
     //test cercle
     printf("\n");
-    printf("test sur le COR  cerlce\n");
+    printf("********************test sur le COR  cerlce********************\n");
     Cercle cercleCOR(1, 2, 1, 0xFF0000);
     cout <<cercleCOR << endl;
     ifstream fichier2("COR/FichiersTest/TestCORCercle.txt");
@@ -140,7 +170,7 @@ int main(){
 
     //test polygone
     printf("\n");
-    printf("test sur le COR  polygone\n");
+    printf("********************test sur le COR  polygone********************\n");
     Polygone polygoneCOR(0xFF0000);
     polygoneCOR.addPoint(Vecteur2D(1,1));
     polygoneCOR.addPoint(Vecteur2D(1,2));
@@ -154,7 +184,7 @@ int main(){
 
     //test forme complexe
     printf("\n");
-    printf("test sur le COR  forme complexe\n");
+    printf("********************test sur le COR  forme complexe********************\n");
     FormeComplexe formeComplexeCOR(0x000000);
     formeComplexeCOR.ajouterForme(&cercleCOR);
     formeComplexeCOR.ajouterForme(&segmentCOR);
@@ -166,5 +196,8 @@ int main(){
         cout << *f << endl;
     }
 
+    /**************************************************************************
+    *  FIN
+    *************************************************************************/
     return 0;
 }
